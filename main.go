b543a3160a220ef80bfd5c17d1d1b1bc7ef10714
mse99 <mohamedsalemedrah@gmail.com>
2025-02-25 23:51:58 +0200

    Install fiber and create a hello world app

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	app := fiber.New()
+
+	app.Get("/status", func(c *fiber.Ctx) error {
+		return c.Status(http.StatusOK).Send([]byte("OK"))
+	})
+
+	go app.Listen(":3000")
+
+	<-ctx.Done()
+
+	err := app.ShutdownWithTimeout(time.Second * 30)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
