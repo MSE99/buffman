031368443c5ee9bf35907b60ee16e8218f3f3955
mse99 <mohamedsalemedrah@gmail.com>
2025-03-07 21:29:05 +0200

    Move handlers code in `web/server.go` to `web/controllers.go`

---
+++ web/controllers.go
@@ -0,0 +1,46 @@
+package web
+
+import (
+	"context"
+	"crypto/sha256"
+	"crypto/subtle"
+	"database/sql"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mse99/buffman/buffman"
+	"github.com/mse99/buffman/config"
+)
+
+func handleGetStatusRequest(ctx *fiber.Ctx) error {
+	return ctx.Status(200).Send([]byte("OK"))
+}
+
+func createQueueRequestHandler(ctx context.Context, db *sql.DB) func(*fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		token := c.Query("token")
+		hashedToken := sha256.Sum256([]byte(token))
+		hashedOdooSecret := sha256.Sum256([]byte(config.OdooSecret))
+
+		if subtle.ConstantTimeCompare(hashedToken[:], hashedOdooSecret[:]) == 0 {
+			log.Println("received request with invalid secret")
+			return c.Status(http.StatusUnauthorized).Send([]byte("Unauthorized"))
+		}
+
+		payload := string(c.Body())
+
+		if len(payload) == 0 {
+			return c.Status(http.StatusBadRequest).Send([]byte("Invalid body sent"))
+		}
+
+		queueErr := buffman.QueueRequest(ctx, db, buffman.Request{Payload: payload, CreatedOn: time.Now()})
+		if queueErr != nil {
+			log.Println(queueErr)
+			return c.Status(http.StatusInternalServerError).Send([]byte(""))
+		}
+
+		return c.Status(http.StatusOK).Send([]byte("OK"))
+	}
+}
--- web/server.go
+++ web/server.go
@@ -2,18 +2,11 @@ package web
 
 import (
 	"context"
-	"crypto/sha256"
-	"crypto/subtle"
 	"database/sql"
-	"log"
-	"net/http"
 	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/mse99/buffman/buffman"
-	"github.com/mse99/buffman/config"
 )
 
 func CreateServer(ctx context.Context, db *sql.DB) *fiber.App {
@@ -25,33 +18,6 @@ func CreateServer(ctx context.Context, db *sql.DB) *fiber.App {
 func setupRouter(ctx context.Context, app *fiber.App, db *sql.DB) {
 	app.Use(logger.New(logger.Config{Output: os.Stdout}))
 
-	app.Get("/status", func(c *fiber.Ctx) error {
-		return c.Status(http.StatusOK).Send([]byte("OK"))
-	})
-
-	app.Post("/", func(c *fiber.Ctx) error {
-		token := c.Query("token")
-		hashedToken := sha256.Sum256([]byte(token))
-		hashedOdooSecret := sha256.Sum256([]byte(config.OdooSecret))
-
-		if subtle.ConstantTimeCompare(hashedToken[:], hashedOdooSecret[:]) == 0 {
-			log.Println("received request with invalid secret")
-			return c.Status(http.StatusUnauthorized).Send([]byte("Unauthorized"))
-		}
-
-		payload := string(c.Body())
-
-		if len(payload) == 0 {
-			return c.Status(http.StatusBadRequest).Send([]byte("Invalid body sent"))
-		}
-
-		queueErr := buffman.QueueRequest(ctx, db, buffman.Request{Payload: payload, CreatedOn: time.Now()})
-		if queueErr != nil {
-			log.Println(queueErr)
-			return c.Status(http.StatusInternalServerError).Send([]byte(""))
-		}
-
-		return c.Status(http.StatusOK).Send([]byte("OK"))
-	})
-
+	app.Get("/status", handleGetStatusRequest)
+	app.Post("/", createQueueRequestHandler(ctx, db))
 }
