e459cbdbd6def7188a1757f5f5c6104fbb95c727
mse99 <mohamedsalemedrah@gmail.com>
2025-03-07 21:03:00 +0200

    Port `dispatcher.go` to `buffman` package

--- buffman/buffman.go
+++ buffman/buffman.go
@@ -1,13 +1,21 @@
 package buffman
 
-import "context"
+import (
+	"context"
+	"database/sql"
+)
 
-func StartDispatchToFMA(ctx context.Context) error {
+func StartDispatchToFMA(ctx context.Context, db *sql.DB) error {
 	tk, err := newFmaToken(ctx)
 	if err != nil {
 		return err
 	}
 	go tk.waitAndRefresh()
+
+	go processStoredRequests(ctx, requestProcessingOpts{
+		db: db,
+		tk: tk,
+	})
 
 	return nil
 }
