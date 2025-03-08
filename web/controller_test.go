c3ba3eb2a80bab5e09506dbcb757732530e4eb23
mse99 <mohamedsalemedrah@gmail.com>
2025-03-08 02:30:15 +0200

    Add tests for handle queue request route

--- web/controller_test.go
+++ web/controller_test.go
@@ -2,26 +2,37 @@ package web
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/mse99/buffman/config"
 	"github.com/mse99/buffman/repos"
 )
 
 var ctx = context.Background()
 
-func TestStatusEndpoint(t *testing.T) {
-	t.Parallel()
-
+func createTestingServer(t *testing.T) (*fiber.App, *sql.DB) {
 	db, err := repos.ConnectToDB(ctx, ":memory:")
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer db.Close()
+	t.Cleanup(func() { db.Close() })
 
 	server := CreateServer(context.Background(), db)
-	defer server.Shutdown()
+	t.Cleanup(func() { server.Shutdown() })
+
+	return server, db
+}
+
+func TestStatusEndpoint(t *testing.T) {
+	t.Parallel()
+
+	server, _ := createTestingServer(t)
 
 	req := httptest.NewRequest(http.MethodGet, "/status", nil)
 
@@ -32,4 +43,51 @@ func TestStatusEndpoint(t *testing.T) {
 	} else if res.StatusCode != http.StatusOK {
 		t.Errorf("expected status 200 but got %d", res.StatusCode)
 	}
+}
+
+func TestHandleQueueRequest(t *testing.T) {
+	config.OdooSecret = "HelloWorld"
+
+	t.Run("InvalidSecret", func(t *testing.T) {
+		server, _ := createTestingServer(t)
+
+		req := httptest.NewRequest(http.MethodPost, "/", nil)
+		res, resErr := server.Test(req)
+
+		if resErr != nil {
+			t.Error(resErr)
+		} else if res.StatusCode != http.StatusUnauthorized {
+			t.Errorf("expected status 401 but got %d", res.StatusCode)
+		}
+	})
+
+	t.Run("ValidSecretButInvalidBody", func(t *testing.T) {
+		server, _ := createTestingServer(t)
+
+		path := fmt.Sprintf("/?token=%s", config.OdooSecret)
+
+		req := httptest.NewRequest(http.MethodPost, path, nil)
+		res, resErr := server.Test(req)
+
+		if resErr != nil {
+			t.Error(resErr)
+		} else if res.StatusCode != http.StatusBadRequest {
+			t.Errorf("expected status 400 but got %d", res.StatusCode)
+		}
+	})
+
+	t.Run("HappyPath", func(t *testing.T) {
+		server, _ := createTestingServer(t)
+
+		path := fmt.Sprintf("/?token=%s", config.OdooSecret)
+
+		req := httptest.NewRequest(http.MethodPost, path, strings.NewReader("HelloWorld"))
+		res, resErr := server.Test(req)
+
+		if resErr != nil {
+			t.Error(resErr)
+		} else if res.StatusCode != http.StatusOK {
+			t.Errorf("expected status 200 but got %d", res.StatusCode)
+		}
+	})
 }
